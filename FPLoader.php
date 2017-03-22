01be2df85d445a024be61fd3bdc23d3709e7113c
Umesh Ghimire <me.umeshuser@gmail.com>
2017-03-22 13:41:52 +0545

    Package management

--- FPLoader.php
+++ FPLoader.php
@@ -3,18 +3,77 @@
 class FPLoader
 {
 
+    private $registeredPackages = array();
+
     public function __construct()
     {
 
-        spl_autoload_register(array($this, 'autoloader'));
+        spl_autoload_register(array($this, 'Autoloader'));
+
+        $this->RegisterAndLoadPackages();
+
     }
 
-
-    private function autoloader()
+    function Autoloader()
     {
 
 
     }
+
+    // Get instance of package
+    private function GetInstance()
+    {
+
+
+    }
+
+    private function RegisterPackages($packages = array())
+    {
+
+        if (count(array_keys($packages)) != count(array_values($packages))) {
+
+            throw  new Exception("package key value missmatch.");
+        }
+
+        $this->registeredPackages = $packages;
+
+        // add_action('init', array($this, 'register_shortcodes'));
+
+    }
+
+// Register and auto load packages
+    private function RegisterAndLoadPackages()
+    {
+
+        $this->RegisterPackages(array(
+
+                "shortcode" => "FPShortCode" // Key == Package directory name and Value is Package Main class name
+
+            )
+        );
+
+        $this->LoadPackage("shortcode");
+
+    }
+
+    private function LoadPackage($packageName)
+    {
+        try {
+
+            if (!isset($this->registeredPackages[$packageName])) {
+
+                throw  new Exception("Package not registered yet.");
+
+            }
+
+        } catch (Exception $e) {
+
+
+        }
+
+    }
+
+
 }
 
 ?>
